9f2a1325ec90e77b659d9d41098c1acfb9b04440
John Duffy <john.duffy34@mail.dcu.ie>
2019-02-07 18:05:35 +0000

    Update main.dart

--- code/main.dart
+++ code/main.dart
@@ -111,25 +111,11 @@ class MyHomePage extends StatelessWidget {
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.only(
-                        left: 20.0, right: 5.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFF4364A1),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("FaceBook",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 10.0, right: 20.0, top: 10.0),
+                        left: 20.0, right: 20.0, top: 10.0),
                     child: new Container(
                         alignment: Alignment.center,
                         height: 60.0,
